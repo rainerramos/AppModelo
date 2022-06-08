1db0436f060fa7c8580385298f5f23e84f54e510
Rainer <rainermr89@hotmail.com>
2022-06-08 13:35:14 -0300

    Custom Tag Helpers (Inserindo tipo de parâmetro)

--- src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
+++ src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
@@ -5,11 +5,12 @@ namespace DevIO.UI.Site.Extensions
 {
     public class EmailTagHelper : TagHelper
     {
+        public string EmailDomain { get; set; } = "desenvolvedor.io";
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + "desenvolvedor.io";
+            var target = content.GetContent() + "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto:" + target);
             output.Content.SetContent(target);
         }
