75a7aa6ed876fc8165fde6815a94f3025289d499
Rainer <rainermr89@hotmail.com>
2022-06-08 13:26:31 -0300

    Custom Tag Helpers

---
+++ src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "desenvolvedor.io";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
