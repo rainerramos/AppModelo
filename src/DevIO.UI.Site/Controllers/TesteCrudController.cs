4d59067517f32a8f840db4ebe27af9ad40be9eee
Rainer <rainermr89@hotmail.com>
2022-06-21 13:03:14 -0300

    Operações CRUD

---
+++ src/DevIO.UI.Site/Controllers/TesteCrudController.cs
@@ -0,0 +1,43 @@
+﻿using DevIO.UI.Site.Data;
+using DevIO.UI.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class TesteCrudController : Controller
+    {
+        private readonly MeuDbContext _contexto;
+
+        public TesteCrudController(MeuDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno
+            {
+                Nome = "Rainer",
+                DataNascimento = System.DateTime.Now,
+                Email = "rainermr89@hotmail.com"
+            };
+
+            _contexto.Alunos.Add(aluno);
+            _contexto.SaveChanges();
+
+            var aluno2 = _contexto.Alunos.Find(aluno.Id);
+            var aluno3 = _contexto.Alunos.FirstOrDefault(a=>a.Email == "rainermr89@hotmail.com");
+            var aluno4 = _contexto.Alunos.Where(a => a.Nome == "Rainer");
+
+            aluno.Nome = "João";
+            _contexto.Alunos.Update(aluno);
+            _contexto.SaveChanges();
+
+            _contexto.Alunos.Remove(aluno);
+            _contexto.SaveChanges();
+
+            return View();
+        }
+    }
+}
